d2a3fc033704f2489919efbb21add23c2a1f0a4e
Gois <mikegois@gmail.com>
2024-12-02 12:46:45 +0000

    Dataset

--- CarPlateDetection.py
+++ CarPlateDetection.py
@@ -1,15 +1,11 @@
 import numpy as np
-from PIL import Image, ImageDraw
+import os
+import cv2
 import matplotlib.pyplot as plt
 from ultralytics import YOLO
 import kagglehub
 
 model = YOLO("yolo11n.pt")
 
-results = model.train(data="coco8.yaml", epochs=100, imgsz=640)
+results = model.train(data="CarPlates/Lamine.yaml", epochs=20, imgsz=640)
 
-
-""" # Download latest version
-path = kagglehub.dataset_download("andrewmvd/car-plate-detection")
-
-print("Path to dataset files:", path) """

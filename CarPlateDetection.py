52ac7ae1109b2a2adac570dac8bbfdf57830dce3
MigGois <mgsli@iscte-iul.pt>
2025-01-16 01:46:21 +0000

    TrainedModel

--- CarPlateDetection.py
+++ CarPlateDetection.py
@@ -6,11 +6,11 @@ from ultralytics import YOLO
 import math
 from paddleocr import PaddleOCR
 import re
-
+import time
+from imutils import contours
 
 IMG_DATA = "dataset/test/images/a3ad91fabd188be3.jpg"
-LABELA_DATA = "dataset/test/labels/a3ad91fabd188be3.txt"
-
+BLACKLIST = ['AL', 'AND', 'A', 'BY', 'B', 'BIH', 'BG', 'HR', 'CY', 'CZ', 'DK', 'EST', 'FIN', 'F', 'D', 'GR', 'GB', 'H', 'IS', 'IRL', 'I', 'LV', 'FL', 'LT', 'L', 'M', 'MD', 'MC', 'MNE', 'NL', 'NMK', 'N', 'PL', 'P', 'RO', 'RSM', 'SRB', 'SK', 'SLO', 'E', 'S', 'UA', 'UK', 'V']
 
 ocr = PaddleOCR(use_angle_cls = True, lang='en', show_log=False)
 
@@ -52,6 +52,28 @@ def calculate_iou(bbox1, bbox2):
     return inter_area / union_area if union_area > 0 else 0
 
 
+def find_iou(plateCoords, label_path, width, height):
+
+    plateCoords = [plateCoords[0], plateCoords[1], plateCoords[2] - plateCoords[0], plateCoords[3] - plateCoords[1]] 
+    
+    best_iou = 0
+
+    with open(label_path, 'r') as f:
+        lines = f.readlines()
+
+    for line in lines:
+
+        bbox = preprocess_bbox(line, height, width)
+        bbox = [bbox[0], bbox[1], bbox[2] - bbox[0], bbox[3] - bbox[1]]
+
+        iou = calculate_iou(plateCoords, bbox)
+
+        if iou > best_iou:
+            best_iou = iou
+
+    return best_iou
+
+
 def plateDetection(model, imgPath):
 
     results = model.predict(source=imgPath)
@@ -62,88 +84,18 @@ def plateDetection(model, imgPath):
     return xyxys # [x1, y1, x2, y2]
 
 
-def order_points(pts):
-
-    center = np.mean(pts) # Step 1: Find centre of object
-
-    shifted = pts - center # Step 2: Move coordinate system to centre of object
-
-    theta = np.arctan2(shifted[:, 0], shifted[:, 1]) # Step 3: Find angles subtended from centroid to each corner point
-
-    ind = np.argsort(theta) # Step 4: Return vertices ordered by theta
-
-    return pts[ind]
-
-
-def getContours(img, orig):  
-    biggest = np.array([])
-    maxArea = 300
-    imgContour = orig.copy()  
-    contours, hierarchy = cv2.findContours(img, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
-    index = None
-    for i, cnt in enumerate(contours):
-        area = cv2.contourArea(cnt)
-        peri = cv2.arcLength(cnt, True)
-        approx = cv2.approxPolyDP(cnt,0.02*peri, True)
-        
-
-        if area > maxArea and len(approx) == 4:
-            biggest = approx
-            maxArea = area
-            index = i 
-
-
-    warped = None  
-    if index is not None: 
-        cv2.drawContours(imgContour, contours, index, (255, 0, 0), 3)
-
-        src = np.squeeze(biggest).astype(np.float32) 
-        height = orig.shape[0]
-        width = orig.shape[1]
-        
-        dst = np.float32([[0, 0], [0, height - 1], [width - 1, 0], [width - 1, height - 1]])
-
-        biggest = order_points(src)
-        dst = order_points(dst)
-
-        M = cv2.getPerspectiveTransform(src, dst)
-
-        phi = math.atan2(M[2][1], M[2][2])
-        psi = math.atan2(M[1][0], M[0][0])
-
-        if math.cos(psi) == 0:
-            thetaa = math.atan2(-M[2][0], (M[1][0]/math.sin(psi)))
-        else:
-            thetaa = math.atan2(-M[2][0], (M[0][0]/math.cos(psi)))
-
-        pi = 22/7
-
-        phid = phi*(180/pi)
-        thetaad = thetaa*(180/pi)
-        psid = math.degrees(psi)
-
-        M = cv2.getPerspectiveTransform(src, dst)
-
-        if abs(psid) > 35:
-            psid = 0
-            print("Not Rotating")
-            M = np.zeros((3, 3))
-
-        img_shape = (width, height)
-        warped = cv2.warpPerspective(orig, M, img_shape, flags=cv2.INTER_LINEAR)
- 
-    return biggest, imgContour, warped 
-
     
 def post_processing(image):
 
-    img_h, img_w, _ = image.shape
+    """ img_h, img_w, _ = image.shape
 
-    if img_h < 640:
-        scale = round(640/img_h)
-        image = cv2.resize(image, (img_w * scale, img_h * scale), interpolation = cv2.INTER_LANCZOS4)
-
-    return image
+    if img_h < 500:
+        scale = round(600/img_h)
+        image = cv2.resize(image, (img_w * scale, img_h * scale), interpolation = cv2.INTER_LANCZOS4) """
+    
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    
+    return gray
 
 
 def image_processing(path):
@@ -170,28 +122,7 @@ def image_processing(path):
 
         carPlate = img[plateCoord[1]:plateCoord[3], plateCoord[0]:plateCoord[2]]
 
-        kernel = np.ones((3,3))
-
-        img_gray = cv2.cvtColor(carPlate, cv2.COLOR_RGB2GRAY) 
-
-        imgBlur = cv2.GaussianBlur(img_gray,(5,5),1)
-
-        imgCanny = cv2.Canny(imgBlur,100,200)
-
-        imgDila = cv2.dilate(imgCanny, kernel,iterations=2)
-
-        imgThres = cv2.erode(imgDila, kernel, iterations=2)
-
-        _, _, warped = getContours(imgThres, carPlate)
-
-        notWarped = False
-
-        if warped is None:
-            warped = carPlate.copy()
-            notWarped = True
-
-
-        result = post_processing(warped)
+        result = post_processing(carPlate)
         
         result1 = ocr.ocr(result, cls=True)
 
@@ -199,24 +130,21 @@ def image_processing(path):
 
         test = result1[0]
 
-        if test is None and not notWarped:
-            result = post_processing(carPlate)
-            result1 = ocr.ocr(result, cls=True)
-            test = result1[0]
-
         if test is not None:
             for res in test:
-                data += str(res[1][0])
+                word = str(res[1][0])
+                if not(len(word) <= 3 and word in BLACKLIST):
+                    data += str(res[1][0])
 
         data = re.sub(r'[^a-zA-Z0-9]', '', data)
-
         
-        processingResults.append([result, data])
+        processingResults.append([result, data, plateCoord])
 
-    return processingResults
+    return processingResults, img1
 
 
 def plate_accuracy():
+
     files = os.listdir("dataset/test/images")
 
     totalPlates = 0
@@ -224,6 +152,11 @@ def plate_accuracy():
     foundPlates = 0
 
     correctPlates = 0
+
+    tp, fp, fn = 0, 0, 0
+
+    start_time = time.time()
+    
 
     for file in files:
 
@@ -233,7 +166,11 @@ def plate_accuracy():
 
         label_path = "dataset/test/plateLabels/" + file
 
-        results = image_processing(img_path)
+        loc_plate_path = "dataset/test/labels/" + file
+
+        results, img1 = image_processing(img_path)
+
+        img_h, img_w, _ = img1.shape
 
         lines = []
 
@@ -248,30 +185,62 @@ def plate_accuracy():
             
             foundPlates += 1
 
+            iou = find_iou(result[2], loc_plate_path, img_w, img_h)
+
+            if iou > 0.8:
+                tp += 1
+            else:
+                fp += 1
+
             for line in lines:
+
+                #print("Resultado Modelo:", result[1].upper())
+                #print("Ground Truth:", line.upper().strip())
 
                 if result[1].upper() == line.upper().strip():
                     correctPlates += 1
+                    break
 
-    fp = round((foundPlates/totalPlates) * 100, 2)
+    end_time = time.time()
+
+    fn = totalPlates - tp
+
+    elapsed_time = end_time - start_time
+    time_image = (elapsed_time/168) * 1000
+
+    precision = tp / (tp + fp) if (tp + fp) > 0 else 0
+    recall = tp / (tp + fn) if (tp + fn) > 0 else 0
+    f1_score = 2 * (precision * recall) / (precision + recall) if (precision + recall) > 0 else 0
+
     cp = round((correctPlates/foundPlates) * 100, 2)
 
-    print("FoundPlates:", fp, "%")
-    print("CorrectPlates:", cp, "%")                    
+
+    print("PlateDetection".center(24, "-"))
+    print(f"True Positive: {tp}")
+    print(f"False Positive: {fp}")
+    print(f"False Negative: {fn}")
+    print(f"Precision: {precision * 100:.2f}%")
+    print(f"Recall: {recall * 100:.2f}%")
+    print(f"F1-Score: {f1_score * 100:.2f}%")
+
+    print()
+    print("TextDetection".center(24, "-"))
+    print(f"CorrectPlates: {cp}%")    
+
+    print()
+    print("Time".center(24, "-"))
+    print(f"Elapsed time: {elapsed_time:.2f} seconds")
+    print(f"Time per Image: {time_image:.2f} ms")              
 
 
-def plot_labeled_data(mode='train'):
+def plot_labeled_data(path):
 
     fig = plt.figure(figsize=(20, 20)) 
     rows = 4
     columns = 4
     plot_index = 1
 
-    results = image_processing(IMG_DATA)
-
-    image = cv2.imread(IMG_DATA)
-
-    image1 = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    results,orig = image_processing(path)
 
     for result in results:
         ax = fig.add_subplot(rows, columns, plot_index)
@@ -284,7 +253,7 @@ def plot_labeled_data(mode='train'):
         plot_index += 1
 
     ax = fig.add_subplot(rows, columns, plot_index)
-    ax.imshow(image1)
+    ax.imshow(orig)
     ax.set_title(f"Original Image")
 
     plt.show()
@@ -294,54 +263,6 @@ if __name__ == "__main__":
     model = YOLO("runs/detect/train/weights/best.pt")
     #model = YOLO("yolo11s.pt")
     #results = model.train(data="Lamine.yaml", epochs=100, imgsz=640, device=0)
-    #plot_labeled_data()
+    #plot_labeled_data("dataset/test/images/d8daed582e6cce2d.jpg")
     plate_accuracy()
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-""" def iou():
-    results = model.predict(source=imgPath)
-    img_h, img_w, _ = img.shape
-
-    for result in results:
-
-        boxes = result.boxes.cpu().numpy()
-        xyxys = boxes.xyxy
-        class_ids = boxes.cls.astype(int)
-
-        for xyxy, class_id in zip(xyxys, class_ids):
-            print("Class id: ", class_id)
-            bbox1 = [xyxy[0], xyxy[1], xyxy[2] - xyxy[0], xyxy[3] - xyxy[1]]
-
-            best_iou = 0
-            best_idx = -1
-            for d in data:
-                [x1, y1, x2, y2] = preprocess_bbox(d, img_h, img_w)
-                iou = calculate_iou(bbox1, [x1, y1, x2 - x1, y2 - y1])
-                if iou > best_iou:
-                    best_iou = iou
-                    best_idx = data.index(d)
-
-            print("Best iou: ", best_iou) """
